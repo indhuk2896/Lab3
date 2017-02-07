bba0bc383e44ff5692a2850f17551156445ab35c
ITLAB <ITLAB@IT>
2017-02-07 14:29:24 +0530

    second commit

--- src/com/example/basgra/MainActivity.java
+++ src/com/example/basgra/MainActivity.java
@@ -1,35 +1,77 @@
 package com.example.basgra;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
-
+	
+	DemoView demo;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        demo = new DemoView(this);
+		setContentView(demo);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	private class DemoView extends View {
+		public DemoView(Context mcontext) {
+			super(mcontext);
+		}
+    
+		@Override
+		protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+ 
+			Paint paint = new Paint();
+			// custom drawing code here
+			
+			paint.setStyle(Paint.Style.FILL_AND_STROKE);
+ 
+				//black background
+			paint.setColor(Color.BLACK);
+			canvas.drawPaint(paint);
+ 
+				//moon
+			paint.setAntiAlias(false);
+			paint.setColor(Color.WHITE);
+			canvas.drawCircle(30, 30, 50, paint);
+ 
+			//moon
+			paint.setAntiAlias(false);
+			paint.setColor(Color.WHITE);
+			canvas.drawCircle(30, 30, 50, paint);
+			
+			/*	// draw green circle with anti aliasing turned on
+			paint.setAntiAlias(true);
+			paint.setColor(Color.GRAY);
+			canvas.drawCircle(60, 20, 15, paint); */
+ 
+			// draw red rectangle with anti aliasing turned off
+			paint.setAntiAlias(false);
+			paint.setColor(Color.BLUE);
+			canvas.drawRect(0, 400, 500,320, paint);
+ 
+			/*// draw the rotated text
+			canvas.rotate(-40);
+ 
+			paint.setStyle(Paint.Style.FILL);
+			canvas.drawText("Graphics Rotation", 45, 185, paint);
+ 
+			// undo the rotate
+			canvas.restore();*/
+		}
+		
+		
+	}
+    
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
